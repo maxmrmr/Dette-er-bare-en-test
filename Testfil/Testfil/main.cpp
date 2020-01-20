d2d6d04946cff50f44d533de39704d68158c9c70
Max Robertson <maxmr@stud.ntnu.no>
2020-01-20 10:11:30 +0100

    første commit

---
+++ Testfil/Testfil/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Testfil
+//
+//  Created by Max Robertson on 20/01/2020.
+//  Copyright © 2020 Max Robertson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
